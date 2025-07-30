151a3025463b194435b7ad62b9fd0c5433093753
xll <1879845580@qq.com>
2025-07-30 11:23:50 +0800

    Add files via upload
    Signed-off-by: xll <1879845580@qq.com>

---
+++ train_yolov5s.py
@@ -0,0 +1,17 @@
+import warnings
+warnings.filterwarnings('ignore')
+from ultralytics import YOLO
+if __name__ == '__main__':
+  model = YOLO('ultralytics/cfg/models/v5/yolov5.yaml')
+  model.load('yolov5s.pt')  #注释则不加载
+  results = model.train(
+    data='data.yaml',  #数据集配置文件的路径
+    epochs=200,  #训练轮次总数
+    batch=16,  #批量大小，即单次输入多少图片训练
+    imgsz=800,  #训练图像尺寸
+    workers=8,  #加载数据的工作线程数
+    device = 'cuda', #指定训练的计算设备，无nvidia显卡则改为 'cpu'
+    optimizer='SGD',  #训练使用优化器，可选 auto,SGD,Adam,AdamW 等
+    amp= True,  #True 或者 False, 解释为：自动混合精度(AMP) 训练
+    cache=False  # True 在内存中缓存数据集图像，服务器推荐开启
+)
